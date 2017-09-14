77b3b70e97f6a44bde515903bd489f39bc6dcb15
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2017-09-14 22:56:12 +0800

    Add biology test questions

--- quiz.py
+++ quiz.py
@@ -1,5 +1,71 @@
-biology = [
-    ['what are the products of aerobic respiration?', 'carbon dioxide and water'],
-    ['what are the produts of anaerobic respiration in yeast?', 'ethanol and carbon dioxide'],
-    ['what are the products of anaerobic respiration in humans?', 'lactic acid']
-]
+biology = {
+    'respiration': [
+        ["What are the products of aerobic respiration?", "carbon dioxide and water"],
+        ["What are the products of anaerobic respiration in yeast?", "ethanol and carbon dioxide"],
+        ["What are the products of anaerobic respiration in humans?", "lactic acid"],
+        ["What happens during inhalation?", "external intercostal muscles contract\ninternal intercostal muscles relax\nribs and sternum move up and out\ndiaphragm contracts and flattens\nchest cavity volume increases\npressure decreases\nair rushes in"],
+        ["What happens during exhalation?", "external intercostal muscles relax\ninternal intercostal muscles contract\nribs and sternum return to original position\ndiaphagm relaxes and arches\nchest cavity volume decreases\npressure increases\nair rushes out"],
+        ["What is the composition of nitrogen in inhaled and exhaled air?", "78% and 78%"],
+        ["What is the composition of oxygen in inhaled and exhaled air?", "20% and 16%"],
+        ["What is the composition of carbon dioxide in inhaled and exhaled air?", "0.03% and 4%"],
+        ["What is the composition of water vapour in inhaled and exhaled air?", "variable and saturated"],
+        ["What is the composition of dust in inhaled and exhaled air?", "variable and little"],
+        ["What is the temperature of inhaled and exhaled air?", "variable and body temperature"],
+        ["What adaptations does the nose have?", "hair, mucus, sensory cells"],
+        ["What adaptations do the alveoli have?", "large surface area to volume ratio, one-cell thick walls, thin film of moisture, network of blood capillaries"],
+        ["What harmful effects does nicotine have on the body?", "addictive\ncauses the release of adrenaline\nmakes blood clot easily"],
+        ["What harmful effects does carbon monoxide have on the body?", "binds irreversibly with haemoglobin to form carboxyhaemoglobin\nincreases the rate of fatty deposites on inner arterial wall -> coronary heart disease"],
+        ["What harmful effects does tar have on the body?", "causes uncontrolled cell division -> cancer\nparalyses cilia lining the air passages -> chronic bronchitis and emphysema"],
+        ["What harmful effects do irritants have on the body?", "paralyses cilia lining the air passages -> chronic bronchitis and emphysema"],
+        ["What happens when a person has chronic bronchitis?", "prolonged exposure to irritant particles\nepithelium of air passages becomes inflamed\nexcessive mucus secreted by epithelium\ncilia become paralysed\nmucus and dust particles cannot be removed\nair passages become blocked\npersistent coughing to clear air passages"],
+        ["What happens when a person has emphysema?", "persistent violent coughing due to bronchitis\npartition walls between aleveoli break down\ndecreased surface area to volume ratio for gaseous exchange\nlungs lose elasticity and become inflated with air\nwheezing and severe breathlessness"],
+    ],
+    'digestion': [
+        ["What chemicals are present in the mouth?", "amylase"],
+        ["What chemicals are present in the stomach?", "hydrochloric acid, pepsinogen, prorennin"],
+        ["What chemicals are present in pancreatic juice?", "lipase, amylase and trypsinogen"],
+        ["What chemicals are present in intestinal juice?", "peptidases, lipase, enterokinase, dissacharide enzymes"],
+        ["Where does carbohydrate digestion occur?", "mouth and small intestine"],
+        ["Where does protein digestion occur?", "stomach and small intestine"],
+        ["Where does fat digestion occur?", "small intestine"],
+        ["What adaptations does the small intestine have?", "folds, villi, microvilli, one-cell thick epithelium, length"],
+        ["What happens to glucose in the body?", "distributed for respiration for energy; excess is converted by liver and muscles to glycogen for storage"],
+        ["What happens to amino acids in the body?", "synthesis of new protoplasm for growth and repair, synthesis of enzymes and hormones; excess is deaminated in the liver"],
+        ["What happens to fats in the body?", "build protoplasm (cell membranes), broken down to provide energy; excess is stored in adipose tissue"],
+        ["What are the functions of the liver?", "protein synthesis, deamination, iron storage, detoxification, bile, glucose regulation"]
+    ],
+    'nutrition in plants': [
+        ["What is the equation for the light-dependent stage of photosynthesis?", "12 H2O -> 6 O2 + 24 H"],
+        ["What is the equation for the light-independent stage of photosynthesis?", "6 CO2 + 24 H -> C6H12O6 + 6 H2O"],
+        ["What is the overall equation for photosynthesis?", "6 CO2 + 12 H2O -> C6H12O6 + 6 O2 + 6 H2O"],
+        ["What is the overall simplified equation for photosynthesis?", "6 CO2 + 6 H2O -> C6H12O6 + 6 O2"],
+        ["Where does photolysis of water occur?", "grana"],
+        ["Where does the reduction of carbon occur?", "stroma"],
+        ["What happens to the stomata in the day?", "photosynthesis\nK+ ions are AT-ed from epidermal cells into GC\nWP becomes more negative\nwater moves in by osmosis\nGC becomes turgid, swollen\nthicker cell wall on one side causes swollen GC to become more curved\nstoma opens"],
+        ["What happens to the stomata in the night?", "K+ ions diffuse out of the GC\nWP becomes less negative\nwater moves out by osmosis\nGC becomes flaccid\nstoma closes"]
+    ],
+    'inheritance': [
+        ["Define gene.", "unit of inheritance"],
+        ["Define gene locus.", "place on chromosome where gene is located"],
+        ["Define allele.", "different forms of the same gene"],
+        ["Define homologous chromosomes.", "chromosomes which have the same sequence of gene loci, same shape and same length"],
+        ["Define dominant allele.", "only 1 copy of the allele is needed to express the phenotype"],
+        ["Define recessive allele.", "2 copies of the allele are needed to express the phenotype"],
+        ["Define codominant alleles.", "both traits are expressed"],
+        ["List out the components needed in a genetic diagram.", "parents (phenotype, genotype, gametes), random fertilisation, offspring (genotype, phenotype), ratio"],
+        ["What can cause a spontaneous mutation in DNA?", "radiation, mutagenic chemicals, viruses"]
+    ],
+    'homeostasis': [
+        ["Define negative feedback", "set norm to be maintained\nreceptor that detects deviation from set norm\ncontrol center that receives nerve signal from receptor and stimulates effectors to initiate response\ncorrective mechanism that brings about reverse effect of stimulus\nfeedback to receptors that set point has been reached"],
+        ["Define homeostasis", "maintenance of a constant internal environment"]
+    ]
+}
+
+geography = {
+    
+}
+
+contents = {
+    'bio': biology,
+    'geog': geography
+}
