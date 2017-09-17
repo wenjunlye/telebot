7bc2dc23b065c3dcbc4ffa009a4e4f10580f796d
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2017-09-17 23:03:07 +0800

    Update questions list

--- quiz.py
+++ quiz.py
@@ -20,7 +20,7 @@ biology = {
         ["What happens when a person has chronic bronchitis?", "prolonged exposure to irritant particles\nepithelium of air passages becomes inflamed\nexcessive mucus secreted by epithelium\ncilia become paralysed\nmucus and dust particles cannot be removed\nair passages become blocked\npersistent coughing to clear air passages"],
         ["What happens when a person has emphysema?", "persistent violent coughing due to bronchitis\npartition walls between aleveoli break down\ndecreased surface area to volume ratio for gaseous exchange\nlungs lose elasticity and become inflated with air\nwheezing and severe breathlessness"],
     ],
-    'digestion': [
+    'nutrition in man': [
         ["What chemicals are present in the mouth?", "amylase"],
         ["What chemicals are present in the stomach?", "hydrochloric acid, pepsinogen, prorennin"],
         ["What chemicals are present in pancreatic juice?", "lipase, amylase and trypsinogen"],
@@ -58,14 +58,81 @@ biology = {
     'homeostasis': [
         ["Define negative feedback", "set norm to be maintained\nreceptor that detects deviation from set norm\ncontrol center that receives nerve signal from receptor and stimulates effectors to initiate response\ncorrective mechanism that brings about reverse effect of stimulus\nfeedback to receptors that set point has been reached"],
         ["Define homeostasis", "maintenance of a constant internal environment"]
+    ],
+    'eye': [
+        ["From outside in, what are the three layers of the eyeball?", "sclera, choroid and retina"],
+        ["What is the function of the sclera?", "protect eyeball from mechanical damage"],
+        ["What is the function of the choroid?", "prevent internal reflection of light and has blood vessels to transport nutrients and waste products"],
+        ["What is the function of the retina?", "contains photoreceptors to detect light"],
+        ["What is the function of the vitreous chamber?", "keep eyeball firm and refract light"],
+        ["What is the function of the conjunctiva?", "secrete mucus to keep eyeball moist"],
+        ["What is the function of the cornea?", "refract light"],
+        ["What is the function of the aqueous chamber?", "keep front of eyeball firm and refract light"],
+        ["What is the function of the ciliary body?", "control curvature and thickness of lens"],
+        ["What is the function of the suspensory ligament?", "attach lens to ciliary body"],
+        ["What is the function of the lens?", "changes shape and thickness to refract light"],
+        ["What is the function of the rectus muscle?", "control movement of eyeball"],
+        ["What is the function of the optic nerve?", "transmit nerve impulses from photoreceptors in retina to brain"],
+        ["What is the function of the pupil?", "allow light to enter eye"],
+        ["What is the function of the iris?", "control amount of light entering the eye"],
+        ["What is the function of the eyelids?", "protect cornea from mechanical damage"],
+        ["What is the function of the eyelashes", "shield the eye from dust particles"],
+        ["What is the function of the tear gland?", "secrete tears to wash away dust particles, keep cornea moist and lubricate conjunctiva"],
+        ["Where is light refracted?", "cornea, aqueous chamber, lens and vitreous chamber"],
+        ["What happens to the iris in bright light?", "circular muscles contract\nradial muscles relax\npupil constricts"],
+        ["What happens to the iris in dim light?", "circular muscles relax\nradical muscles contract\npupil dilates"],
+        ["What happens to the lens when looking at a far object?", "light rays are parallel\nciliary muscles relax\nsuspensory ligament becomes taut\nlens become thinner"],
+        ["What happens to the lens when looking at a near object?", "light rays are divergent\nciliary muscles contract\nsuspensory ligament becomes slack\nlens become rounder"]
     ]
 }
 
 geography = {
-    
+    'tourism': [
+        ["What are the components of an investigation?", "hypothesis, collect data (equipment, procedure), analyse data, present data, conclusion"],
+        ["What are the 3 different kinds of sampling?", "random, systematic, stratified"],
+        ["What are the advantages of random sampling?", "unbiased, can be used with large sample populations"],
+        ["What are the disadvantages of random sampling?", "leads to poor representation if large areas are not hit by the random number generator"],
+        ["What are the advantages of systematic sampling?", "more straightforward, good coverage can be easily achieved"],
+        ["What are the disadvantages of systematic sampling?", "more biased"],
+        ["What are the advantages of stratified sampling?", "more representative of the whole population, saves time"],
+        ["What are the disadvantages of stratified sampling?", "can only be used if sample pool can be divided into categories, may be hard to identify categories effectively"],
+        ["Give an example of ecotourism.", "Waitomo Caves, New Zealand\nglowworms light up the caves\nNew Zealand government partners with local Maori people\nhuman impact is monitored and tour operation closes when carbon dioxide levels are too high\ncarrying capacity ensures that not too many people are in the caves\nvoted best practice in ecotourism"]
+    ],
+    'food resources': [
+        ["What is the increase in land used around the world for GM foods from 1996-2011?", "1.7 million hectares to 160 million hectares"],
+        ["What are the benefits of GM foods?", "increased food supply, increased income for farmers, reduced environmental pollution, improved nutritional value of food"],
+        ["What are some crops that are GM foods?", "soya bean, corn, canola, rice"],
+        ["What qualities do GM foods have?", "have increased resistance to diseases, grow faster"],
+        ["How does GM foods increase income for farmers?", "increased resistance to disease means less crops are damaged and increases output, ability to stay fresh for a longer time allows for export to countries further away and increases income earned"],
+        ["How does GM foods increase food supply?", "increased yield increases supply for food, faster growth leads to more harvests per year, increased shelf life of crops"]
+    ],
+    'health and diseases': [
+        ["What are the social factors causing the spread of malaria?", "poor drainage and stagnant water, improper sanitation, overcrowded living conditions"],
+        ["What are the economic factors causing the spread of malaria?", "limited provision of and access to healthcare"],
+        ["What are the environmental factors causing the spread of malaria?", "effect of climate"],
+        ["What are the social impacts of malaria?", "death rate and infant mortality rate"],
+        ["What are the economic impacts of malaria?", "cost of health care, loss of productivity and burden on households"],
+        ["What are the social factors causing the spread of HIV/AIDS?", "social stigma, lack of education, lifestyle choices, lapses in medical practices"],
+        ["What are the economic factors causing the spread of HIV/AIDS?", "vice trades and mobility"],
+        ["What are the social impacts of HIV/AIDS?", "life expectancy, infant mortality rate and orphan crisis"],
+        ["What are the economic impacts of HIV/AIDS?", "cost of health care, loss of productivity and slower economic growth"],
+        ["What can individuals do to manage infectious diseases?", "exercise social responsibility\ntake preventive measures\nrefrain from risk-taking behaviours\nget vaccinated\nfollow travel advisories"],
+        ["What can communities do to manage infectious diseases?", "community-based mosquito control in Nicaragua\n surveyed residents' understanding of dengue and mosquito-control practices, identified breedings sites\ndengue infection in children fell by 50%\nhouseholds were 25% less likely to have breeding sites\nneeded cooperation from government to deal with water and waste management issues"],
+        ["What precautionary measures can governments implement to manage infectious diseases?", "thermal fogging in Thailand\ndistribute insecticides by using fog\nkills adult mosquitoes outdoors\nexpensive, must be done regularly, reduces visibility, traffic hazard"],
+        ["What mitigation measures can governments implement to manage infectious diseases?", "vector control in Singapore\n'Do the Mozzie Wipeout' community outreach campaign to raise awareness on prevention methods\ncited by WHO has a good role model in preventing and managing dengue cases\nclimate change, new dominant virus Den-1"],
+        ["What can international organisations do to manage infectious diseases?", "UNAIDS Getting to Zero\nbring together resources for coordinated and accountable efforts against AIDS\nsupported Kenya and reduced new HIV infections among children by 44%\nsocial stigma, discrimination"],
+        ["What can non-governmental organisations do to manage infectious diseases?", "International HIV/AIDS Alliance\nadvocates for HIV, health and human rights, increases access to HIV health programmes and supporting other groups responding to HIV\nprovided nearly 1.5 million HIV treatment, care and support packages and reached nearly 380 000 people with programmes addressing stigma and discrimination in 2016"]
+    ]
+}
+
+integrated_humanities = {
+    'un': [
+        
+    ]
 }
 
 contents = {
     'bio': biology,
-    'geog': geography
+    'geog': geography,
+    'ih': integrated_humanities
 }
